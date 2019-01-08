caf6b986c208ca98bfc53e4635a44b341577472c
Leszek Defratyka <leszek.defratyka@gmail.com>
2019-01-08 21:10:29 +0100

    Passing & using data with Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -23,6 +23,8 @@ export class ServersComponent implements OnInit {
   }
   onCreateServer(){
     this.serverCreationStatus = 'Server was created!';
-
   }
+  onUpdateServerName(event: any){
+    console.log(event);
+  } 
 }
